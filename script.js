78cd8e662540976df3adcaa16eb66d76070cec3b
Anggito09 <114100817+Anggito09@users.noreply.github.com>
2026-08-26 16:21:27 +0700

    Bump mobile assets cache version

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (()=>{
   const css=document.createElement('link');
   css.rel='stylesheet';
-  css.href='mobile-enhancements.css?v=20260826';
+  css.href='mobile-enhancements.css?v=20260826-3';
   document.head.appendChild(css);
 
   const load=(src)=>new Promise((resolve,reject)=>{
@@ -12,8 +12,8 @@
     document.head.appendChild(s);
   });
 
-  load('script-core.js')
-    .then(()=>load('experience-period-fix.js'))
-    .then(()=>load('healthcare-slider.js'))
+  load('script-core.js?v=20260826-3')
+    .then(()=>load('experience-period-fix.js?v=20260826-3'))
+    .then(()=>load('healthcare-slider.js?v=20260826-3'))
     .catch(console.error);
 })();
